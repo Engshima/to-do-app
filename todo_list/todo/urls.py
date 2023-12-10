1ae6f45c5245e5d35dae4bf89644956d87064e7e
Engshima <sh.vatanpourasl.com>
2023-12-11 01:59:32 +0330

    finished-view

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('tasks/', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task/create/', CreateTask.as_view(), name='task-create'),
+    path('task/update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task/delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
 ]
--- todo_list/todo/views.py
+++ todo_list/todo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import task
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView 
-from django.views.generic.edit import CreateView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib import messages
 
@@ -29,7 +29,7 @@ class CreateTask(CreateView):
         
         def  form_valid(self, form):
             form.instance.user = self.request.user # set user to the currently logged user
-            messages.success(self.request,"Form successfully submitted'")# create a flash message
+            messages.success(self.request,"Form successfully submitted")# create a flash message
             return super(CreateTask,self).form_valid(form)# return the result of the form_valid() method of the superclass
         
 #In this example, MyCreateView is a view that handles the creation of a new instance of MyModel. 
@@ -39,5 +39,24 @@ class CreateTask(CreateView):
 #and then redirects the user to the "tasks" URL       
         
         
-        
+class TaskUpdate(UpdateView):
+   model = task
+   fields = ['title','description','completed']    
+   success_url = reverse_lazy('todo:tasks') 
+   
+   def form_valid(self,form):
+      
+       messages.success(self.request,"The task was updated successfully.")
+       return super(TaskUpdate,self).form_valid(form)
+   
+   
+class TaskDelete(DeleteView):
+    model = task
+    context_object_name= 'task'
+    success_url = reverse_lazy('todo:tasks')
     
+    
+    def form_valid(self,form):
+      
+       messages.success(self.request,"The task was deleted successfully.")
+       return super(TaskDelete,self).form_valid(form)
